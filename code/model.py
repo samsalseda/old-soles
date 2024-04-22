c3df51b62caa741d7e2defd852efee018309bec4
jakelippert38 <jake_lippert@brown.edu>
2024-04-22 16:27:27 -0400

    please work

---
+++ code/model.py
@@ -0,0 +1,22 @@
+import argparse
+from preprocess import get_feature_dicts
+
+def parse_args(args=None):
+    """ 
+    Perform command-line argument parsing (other otherwise parse arguments with defaults). 
+    To parse in an interative context (i.e. in notebook), add required arguments.
+    These will go into args and will generate a list that can be passed in.
+    For example: 
+        parse_args('--type', 'rnn', ...)
+    """
+    parser = argparse.ArgumentParser(description="Let's train some neural nets!", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--type',           required=True,              choices=['rnn', 'transformer'],     help='Type of model to train')
+    if args is None: 
+        return parser.parse_args()      ## For calling through command line
+    return parser.parse_args(args)      ## For calling through notebook.
+
+def main(self, args):
+    gist, color = get_feature_dicts()
+
+if __name__ == '__main__':
+    main(parse_args())
---
+++ code/preprocess.py
@@ -0,0 +1,13 @@
+import scipy.io as scipy
+
+def get_feature_dicts():
+    gist_filepath = 'data/zappos-gist.mat'
+    color_filepath = 'data/zappos-color.mat'
+
+    gist_dict = scipy.loadmat(gist_filepath)
+    color_dict = scipy.loadmat(color_filepath)
+
+    return gist_dict, color_dict
+
+
+
