91d7d20f0cd422b3a76c89f387e15dc0b4f62de5
davideskilson <david_eskilson@brown.edu>
2024-04-25 19:33:28 -0400

    chore: formatting and outlining for ganblock

--- GANBlock.py
+++ GANBlock.py
@@ -6,13 +6,13 @@ class GANBlock(tf.keras.layers.Layer):
         self.generator = Generator()
         self.discriminator = Discriminator()
 
-    
     def call(self, inputs):
 
-        #Need z sampling of inputs
+        # Need z sampling of inputs
         g = self.generator(inputs)
         d = self.discriminator(g)
         return d
+
 
 class Discriminator(tf.keras.layers.Layer):
 
@@ -21,27 +21,56 @@ class Discriminator(tf.keras.layers.Layer):
         self.hidden_size = 32
 
         leaky_relu_layer = tf.keras.layers.LeakyReLU(negative_slope=0.5)
-        self.disc1 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc2 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 2, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc3 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 4, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc4 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 8, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
+        self.disc1 = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=self.hidden_size, kernel_size=4, strides=2, padding=1
+                ),
+                leaky_relu_layer,
+                tf.keras.layers.SpectralNormalization(),
+            ]
+        )
 
-        self.disc5 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = 1, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer])
+        self.disc2 = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=self.hidden_size * 2, kernel_size=4, strides=2, padding=1
+                ),
+                leaky_relu_layer,
+                tf.keras.layers.SpectralNormalization(),
+            ]
+        )
 
-        self.full_discriminaor = tf.layers.Sequential([self.disc1, self.disc2, self.disc3, self.disc4, self.disc5])
-        
+        self.disc3 = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=self.hidden_size * 4, kernel_size=4, strides=2, padding=1
+                ),
+                leaky_relu_layer,
+                tf.keras.layers.SpectralNormalization(),
+            ]
+        )
+
+        self.disc4 = tf.keras.layers.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=self.hidden_size * 8, kernel_size=4, strides=2, padding=1
+                ),
+                leaky_relu_layer,
+                tf.keras.layers.SpectralNormalization(),
+            ]
+        )
+
+        self.disc5 = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(filters=1, kernel_size=4, strides=2, padding=1),
+                leaky_relu_layer,
+            ]
+        )
+
+        self.full_discriminaor = tf.keras.Sequential(
+            [self.disc1, self.disc2, self.disc3, self.disc4, self.disc5]
+        )
 
     def call(self, inputs):
         return self.full_discriminaor(inputs)
@@ -50,32 +79,7 @@ class Discriminator(tf.keras.layers.Layer):
 class Generator(tf.keras.layers.Layer):
 
     def __init__(self, input_size):
-        self.input_size = input_size
-        self.hidden_size = 32
-
-        leaky_relu_layer = tf.keras.layers.LeakyReLU(negative_slope=0.5)
-        self.disc1 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc2 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 2, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc3 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 4, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-        
-        self.disc4 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = self.hidden_size * 8, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer,
-                                           tf.keras.layers.SpectralNormalization()])
-
-        self.disc5 = tf.layers.Sequential([tf.keras.layers.Conv2d(filters = 1, kernel_size = 4, strides = 2, padding = 1),
-                                           leaky_relu_layer])
-
-        self.full_discriminaor = tf.layers.Sequential([self.disc1, self.disc2, self.disc3, self.disc4, self.disc5])
-        
+        pass
 
     def call(self, inputs):
-        return self.full_discriminaor(inputs)
-    
+        pass
