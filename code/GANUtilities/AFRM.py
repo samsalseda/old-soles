aa65af7c865347fc8d41f1fc59e961a0b6866fc9
jakelippert38 <jake_lippert@brown.edu>
2024-04-25 19:06:59 -0400

    AFRM progress

--- code/GANUtilities/AFRM.py
+++ code/GANUtilities/AFRM.py
@@ -2,8 +2,20 @@ import tensorflow as tf
 
 class AFRM(tf.keras.layers.Layer):
     def __init__(self, size=[256, 4, 4]):
-        self.size = size
-        self.encoder = tf.keras.Sequential([])
+        self.channels, self.heigh, self.width = size
+        self.encoder = tf.keras.Sequential([tf.keras.layers.Conv2d(self.channels, (4, 4, self.channels), strides=2, padding='same'),
+                                            tf.keras.layers.BatchNormalization(),
+                                            tf.keras.layers.ReLU(),
+                                            tf.keras.layers.Conv2d(self.channels, (4, 4, self.channels), strides=2, padding='same'),
+                                            tf.keras.layers.BatchNormalization(),
+                                            tf.keras.layers.ReLU(),
+                                            tf.keras.layers.Conv2d(self.channels, (4, 4, self.channels), strides=2, padding='same'),
+                                            tf.keras.layers.BatchNormalization(),
+                                            tf.keras.layers.ReLU(),
+                                            tf.keras.layers.Conv2d(self.channels, (4, 4, self.channels), strides=2, padding='same'),
+                                            tf.keras.layers.BatchNormalization(),
+                                            tf.keras.layers.ReLU()])
+        self.decoder = tf.keras.Sequential([])
 
     
     def call(self, x):
