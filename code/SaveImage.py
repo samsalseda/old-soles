2e5e98669c46956c2ee61dc9504ef24f203d8b97
Brady Garrison <brady_garrison@brown.edugit config --global user.name bradygarrisonbrown>
2024-05-02 15:23:34 -0400

    Image maker done, but not outputting right thing

--- code/SaveImage.py
+++ code/SaveImage.py
@@ -1,7 +1,14 @@
 import numpy as np
 from PIL import Image
+import os
+from preprocess_temp import process
 
 def save_image(img_array:np.array, name:str):
-    img = Image.fromarray(img_array, 'RGB')
-    img.save("outputs/" + name + ".png", 'PNG')
+    output_dir = "code/outputs/"
+    os.makedirs(output_dir, exist_ok=True)
+    img = Image.fromarray(img_array*255, 'RGB')
+    img.save(output_dir + name + ".png", 'PNG')
 
+if __name__ == "__main__":
+    inputs, outputs = process()
+    save_image(np.asarray(inputs[0]), "testing")
