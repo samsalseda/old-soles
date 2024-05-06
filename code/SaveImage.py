b6939b5048017533ffe127f317bf2e2bb26f1eeb
Brady Garrison <brady_garrison@brown.edugit config --global user.name bradygarrisonbrown>
2024-05-06 12:08:01 -0400

    Working saveimage function

--- code/SaveImage.py
+++ code/SaveImage.py
@@ -1,14 +1,32 @@
 import numpy as np
 from PIL import Image
 import os
-from preprocess_temp import process
+#from preprocess_temp import process
+from pp import load_and_preprocess_image
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 
 def save_image(img_array:np.array, name:str):
+    print(img_array)
     output_dir = "code/outputs/"
     os.makedirs(output_dir, exist_ok=True)
-    img = Image.fromarray(img_array*255, 'RGB')
+    img_array = np.array(img_array * 255, dtype=np.uint8)
+    img = Image.fromarray(img_array, 'RGB')
+
+    imgplot = plt.imshow(img)
+    plt.show()
+
     img.save(output_dir + name + ".png", 'PNG')
 
+
 if __name__ == "__main__":
-    inputs, outputs = process()
-    save_image(np.asarray(inputs[0]), "testing")
+    path = "code/results/201.jpg"
+    img_tensor = load_and_preprocess_image(path, 128)
+    save_image(tf.make_ndarray(tf.make_tensor_proto(img_tensor)), "testing")
+
+    # if __name__ == "__main__":
+#     inputs, outputs = process()
+#     print(inputs[0].dtype)
+#     save_image(tf.make_ndarray(tf.make_tensor_proto(inputs[0])), "testing")
